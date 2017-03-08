3561bcdee2d4d14428938a32c1392f695895bda7
Aleksandar Toplek <aleksandar.toplek@enterwell.net>
2017-03-08 15:39:02 +0100

    Nuget package

--- Enterwell.Clients.Wpf.Notifications/Properties/AssemblyInfo.cs
+++ Enterwell.Clients.Wpf.Notifications/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -8,11 +6,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Enterwell.Clients.Wpf.Notifications")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Enterwell WPF Notifications library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Enterwell d.o.o.")]
 [assembly: AssemblyProduct("Enterwell.Clients.Wpf.Notifications")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Enterwell d.o.o. 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
