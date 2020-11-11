a9543750891fb287854db3cbe277c1802fca235f
Deadpikle <mbabienco@southern.edu>
2020-11-11 17:14:52 -0500

    Update to SDK style project

--- Enterwell.Clients.Wpf.Notifications/Properties/AssemblyInfo.cs
+++ Enterwell.Clients.Wpf.Notifications/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Enterwell.Clients.Wpf.Notifications")]
-[assembly: AssemblyDescription("Enterwell WPF Notifications library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Enterwell d.o.o.")]
-[assembly: AssemblyProduct("Enterwell.Clients.Wpf.Notifications")]
-[assembly: AssemblyCopyright("Copyright © Enterwell d.o.o. 2017-2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -37,17 +25,3 @@ using System.Windows;
                                       //(used if a resource is not found in the page,
                                       // app, or any theme specific resource dictionaries)
 )]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.2.0")]
-[assembly: AssemblyFileVersion("1.4.2.0")]
